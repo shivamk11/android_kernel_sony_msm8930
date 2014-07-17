96cb5a74b09ddbddec9db2124ab600885fc6bf55
shivamk11 <shivamk11@gmail.com>
2014-07-17 14:41:38 +0530

    Add build scripts Signed-off-by: shivamk11 <shivamk11@gmail.com>

---
+++ build-linaro4.7.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+TOOLCHAIN="/home/shivam/development/toolchains/linaro-4.7/bin/arm-cortex_a15-linux-gnueabihf"
+MODULES_DIR="/home/shivam/development/modules"
+ZIMAGE="/home/shivam/development/android_kernel_sony_msm8930_fresh/arch/arm/boot/zImage"
+KERNEL_DIR="/home/shivam/development/android_kernel_sony_msm8930_fresh"
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+rm $ZIMAGE
+rm $MODULES_DIR/*
+fi
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- cyanogenmod_taoshan_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$TOOLCHAIN-strip --strip-unneeded *.ko
+cd $KERNEL_DIR
+else
+echo "Compilation failed! Fix the errors!"
+fi
---
+++ build-linaro4.8.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+TOOLCHAIN="/home/shivam/development/toolchains/linaro-4.8/bin/arm-cortex_a15-linux-gnueabihf"
+MODULES_DIR="/home/shivam/development/modules"
+ZIMAGE="/home/shivam/development/android_kernel_sony_msm8930_fresh/arch/arm/boot/zImage"
+KERNEL_DIR="/home/shivam/development/android_kernel_sony_msm8930_fresh"
+BUILD_START=$(date +"%s")
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+rm $ZIMAGE
+rm $MODULES_DIR/*
+fi
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- cyanogenmod_taoshan_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$TOOLCHAIN-strip --strip-unneeded *.ko
+cd $KERNEL_DIR
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+else
+echo "Compilation failed! Fix the errors!"
+fi
